251997b32a2b42e35ae0ffb1dcb3afe834343792
Suarez Plata Daniel (RBEI/EBS11-SX) <Daniel.SuarezPlata@mx.bosch.com>
2022-02-16 10:17:11 -0600

    Alerter for range added

--- checker.cs
+++ checker.cs
@@ -3,10 +3,45 @@ using System.Diagnostics;
 
 class Checker
 {
+    private static float MAX_TEMPERATURE = 45f;
+    private static float MIN_TEMPERATURE = 45f;
+
+    private static float MAX_STATEOFCHARGE = 80f;
+    private static float MIN_STATEOFCHARGE = 20;
+
+    private static float LIMIT_CHARGERATE = 0.8f;
+
+
+    static bool batteryIsOk(float temperature, float soc, float chargeRate) {
+
+        return isInRange(0f,45f,temperature) && isInRange(20f,80f,soc) && isUnderLimit(0.8f,chargeRate);
+
+    }
+
+    static int Main() {
+        ExpectTrue(batteryIsOk(25, 70, 0.7f));
+        ExpectFalse(batteryIsOk(50, 85, 0.0f));
+        Console.WriteLine("All ok");
+
+        
+        Debug.Assert(isInRange(MIN_TEMPERATURE, MAX_TEMPERATURE, -1.3f) == false,"Temperature under range");
+        Debug.Assert(isInRange(MIN_TEMPERATURE, MAX_TEMPERATURE, 45.5f) == false,"Temperature over range");
+
+        Debug.Assert(isInRange(MIN_STATEOFCHARGE, MAX_STATEOFCHARGE, 19.5f) == false,"State of charge under range");
+        Debug.Assert(isInRange(MIN_STATEOFCHARGE, MAX_STATEOFCHARGE, 80.2f) == false,"State of charge over range");
+
+        Debug.Assert(isUnderLimit(LIMIT_CHARGERATE, 0.75f) == true,"Charge rate under limit");
+        Debug.Assert(isUnderLimit(LIMIT_CHARGERATE, 0.82f) == false,"Charge rate over limit");
+
+
+        return 0;
+    }
 
     static bool isInRange(float minValue, float maxValue, float value){
 
         if(value < minValue || value > maxValue){
+
+            isAtRisk(minValue, maxValue, value);
 
             return false;
         }
@@ -26,11 +61,27 @@ class Checker
 
     }
 
-    static bool batteryIsOk(float temperature, float soc, float chargeRate) {
+    static bool isAtRisk(float minValue, float maxValue, float value)
+    {
+        float warningTolerance = maxValue * 0.05f;
 
-        return isInRange(0f,45f,temperature) && isInRange(20f,80f,soc) && isUnderLimit(0.8f,chargeRate);
+        float upperLimit = maxValue - warningTolerance;
+
+        float lowerLimit = minValue + warningTolerance;
+
+        if (value < lowerLimit || value > upperLimit)
+        {
+            Console.WriteLine("WARNING: Please check the levels of the battery");
+
+            return false;
+        }
+        else
+        {
+            return true;
+        }
 
     }
+
     //This functions "ExpectTrue" and "ExpectFalse" were cloned from the initial commit
     static void ExpectTrue(bool expression) {
         if(!expression) {
@@ -44,21 +95,5 @@ class Checker
             Environment.Exit(1);
         }
     }
-    static int Main() {
-        ExpectTrue(batteryIsOk(25, 70, 0.7f));
-        ExpectFalse(batteryIsOk(50, 85, 0.0f));
-        Console.WriteLine("All ok");
-
-        
-        Debug.Assert(isInRange(0f,45f,-1.3f) == false,"Temperature under range");
-        Debug.Assert(isInRange(0f,45f,45.5f) == false,"Temperature over range");
-
-        Debug.Assert(isInRange(20f,80f,19.5f) == false,"State of charge under range");
-        Debug.Assert(isInRange(20f,80f,80.2f) == false,"State of charge over range");
-
-        Debug.Assert(isUnderLimit(0.8f,0.75f) == true,"Charge rate under limit");
-        Debug.Assert(isUnderLimit(0.8f,0.82f) == false,"Charge rate over limit");
-
-        return 0;
-    }
+    
 }

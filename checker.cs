e577e4f9a0a09de0383801434a82cffdf63242cf
Suarez Plata Daniel (RBEI/EBS11-SX) <Daniel.SuarezPlata@mx.bosch.com>
2022-02-17 12:44:22 -0600

    Test for risk ranges and avoiding hardcoding

--- checker.cs
+++ checker.cs
@@ -4,42 +4,61 @@ using System.Diagnostics;
 class Checker
 {
     private static float MAX_TEMPERATURE = 45f;
-    private static float MIN_TEMPERATURE = 45f;
+    private static float MIN_TEMPERATURE = 0f;
 
     private static float MAX_STATEOFCHARGE = 80f;
     private static float MIN_STATEOFCHARGE = 20;
 
     private static float LIMIT_CHARGERATE = 0.8f;
 
+    private static float TOLERANCE_PERCENTAGE = 0.05f;
 
-    static bool batteryIsOk(float temperature, float soc, float chargeRate) {
+    static bool batteryIsOk(float temperature, float soc, float chargeRate)
+    {
 
-        return isInRange(0f,45f,temperature) && isInRange(20f,80f,soc) && isUnderLimit(0.8f,chargeRate);
+        return isInRange(0f, 45f, temperature) && isInRange(20f, 80f, soc) && isUnderLimit(0.8f, chargeRate);
 
     }
 
-    static int Main() {
+    static int Main()
+    {
         ExpectTrue(batteryIsOk(25, 70, 0.7f));
         ExpectFalse(batteryIsOk(50, 85, 0.0f));
         Console.WriteLine("All ok");
 
-        
-        Debug.Assert(isInRange(MIN_TEMPERATURE, MAX_TEMPERATURE, -1.3f) == false,"Temperature under range");
-        Debug.Assert(isInRange(MIN_TEMPERATURE, MAX_TEMPERATURE, 45.5f) == false,"Temperature over range");
 
-        Debug.Assert(isInRange(MIN_STATEOFCHARGE, MAX_STATEOFCHARGE, 19.5f) == false,"State of charge under range");
-        Debug.Assert(isInRange(MIN_STATEOFCHARGE, MAX_STATEOFCHARGE, 80.2f) == false,"State of charge over range");
+        Debug.Assert(isInRange(MIN_TEMPERATURE, MAX_TEMPERATURE, -1.3f) == false, "Temperature under range");
+        Debug.Assert(isInRange(MIN_TEMPERATURE, MAX_TEMPERATURE, 45.5f) == false, "Temperature over range");
 
-        Debug.Assert(isUnderLimit(LIMIT_CHARGERATE, 0.75f) == true,"Charge rate under limit");
-        Debug.Assert(isUnderLimit(LIMIT_CHARGERATE, 0.82f) == false,"Charge rate over limit");
+        Debug.Assert(isInRange(MIN_STATEOFCHARGE, MAX_STATEOFCHARGE, 19.5f) == false, "State of charge under range");
+        Debug.Assert(isInRange(MIN_STATEOFCHARGE, MAX_STATEOFCHARGE, 80.2f) == false, "State of charge over range");
+
+        Debug.Assert(isUnderLimit(LIMIT_CHARGERATE, 0.75f) == true, "Charge rate under limit");
+        Debug.Assert(isUnderLimit(LIMIT_CHARGERATE, 0.82f) == false, "Charge rate over limit");
+
+        //Test for early warnings extension
+
+        Debug.Assert(isInRiskRange(MIN_TEMPERATURE, MAX_TEMPERATURE, 30f) == false); //Temperature in range
+        Debug.Assert(isInRiskRange(MIN_TEMPERATURE, MAX_TEMPERATURE, 2.2f) == true); //Temperature in range with Warning
+        Debug.Assert(isInRiskRange(MIN_TEMPERATURE, MAX_TEMPERATURE, 43f) == true); // Temperature in range with Warning
+
+
+        Debug.Assert(isInRiskRange(MIN_STATEOFCHARGE, MAX_STATEOFCHARGE, 45) == false); //State of charge in range
+        Debug.Assert(isInRiskRange(MIN_STATEOFCHARGE, MAX_STATEOFCHARGE, 77) == true); //State of charge in range with warning
+        Debug.Assert(isInRiskRange(MIN_STATEOFCHARGE, MAX_STATEOFCHARGE, 23) == true); //State of charge in range with warning
+
+        Debug.Assert(isInRiskLimit(LIMIT_CHARGERATE, 0.4f) == false); //Charge rate in range
+        Debug.Assert(isInRiskLimit(LIMIT_CHARGERATE, 0.77f) == true); //Charge rate in range with warning
 
 
         return 0;
     }
 
-    static bool isInRange(float minValue, float maxValue, float value){
+    static bool isInRange(float minValue, float maxValue, float value)
+    {
 
-        if(value < minValue || value > maxValue){
+        if (value < minValue || value > maxValue)
+        {
 
             isInRiskRange(minValue, maxValue, value);
 
@@ -50,9 +69,11 @@ class Checker
 
     }
 
-    static bool isUnderLimit(float limit, float value){
+    static bool isUnderLimit(float limit, float value)
+    {
 
-        if(value > limit){
+        if (value > limit)
+        {
 
             isInRiskLimit(limit, value);
 
@@ -65,7 +86,7 @@ class Checker
 
     static bool isInRiskRange(float minValue, float maxValue, float value)
     {
-        float warningTolerance = maxValue * 0.05f;
+        float warningTolerance = maxValue * TOLERANCE_PERCENTAGE;
 
         float upperLimit = maxValue - warningTolerance;
 
@@ -73,21 +94,21 @@ class Checker
 
         if (value < lowerLimit || value > upperLimit)
         {
-            Console.WriteLine("WARNING: Please check the levels of the battery");
+            Console.WriteLine("WARNING: Please check the battery levels");
 
-            return false;
+            return true;
         }
         else
         {
-            return true;
+            return false;
         }
 
     }
 
-    
+
     static bool isInRiskLimit(float maxValue, float value)
     {
-        float warningTolerance = maxValue * 0.05f;
+        float warningTolerance = maxValue * TOLERANCE_PERCENTAGE;
 
         float upperLimit = maxValue - warningTolerance;
 
@@ -95,28 +116,32 @@ class Checker
         {
             Console.WriteLine("WARNING: Please check the battery levels");
 
-            return false;
+            return true;
         }
         else
         {
-            return true;
+            return false;
         }
 
     }
 
 
     //This functions "ExpectTrue" and "ExpectFalse" were cloned from the initial commit
-    static void ExpectTrue(bool expression) {
-        if(!expression) {
+    static void ExpectTrue(bool expression)
+    {
+        if (!expression)
+        {
             Console.WriteLine("Expected true, but got false");
             Environment.Exit(1);
         }
     }
-    static void ExpectFalse(bool expression) {
-        if(expression) {
+    static void ExpectFalse(bool expression)
+    {
+        if (expression)
+        {
             Console.WriteLine("Expected false, but got true");
             Environment.Exit(1);
         }
     }
-    
+
 }
